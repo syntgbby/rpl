316a0db337822d9f8b7199df4c230b12afb7ac68
hlyaaulia <hlyaaulia0910@gmail.com>
2025-02-20 20:25:55 +0700

    myprofileupdate

--- app/Views/Aplikan/myprofile.php
+++ app/Views/Aplikan/myprofile.php
@@ -3973,7 +3973,7 @@ License: For each use you must have a valid license purchased only from above li
 												<!--end::Avatar-->
 												<!--begin::Username-->
 												<div class="d-flex flex-column">
-													<div class="fw-bold d-flex align-items-center fs-5">Max Smith 
+													<div class="fw-bold d-flex align-items-center fs-5">Max Smith AA 
 													<span class="badge badge-light-success fw-bold fs-8 px-2 py-1 ms-2">Pro</span></div>
 													<a href="#" class="fw-semibold text-muted text-hover-primary fs-7">max@kt.com</a>
 												</div>
